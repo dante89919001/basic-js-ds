223961333c74946538f9aad89710e572ed73e1b4
Ivan Strizhko <62095188+dante89919001@users.noreply.github.com>
2022-04-27 01:47:49 +0600

    Update remove-from-list.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,7 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
-
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
@@ -14,17 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
+ * Singly - linked lists are already defined with this interface
+ * function ListNode(x) {
+ *   this.value = x;
+ *   this.next = null;
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function removeKFromList(l, k) {
+  let x = l;
+  while (x.next !== null) {
+    if (x.value === k) {
+      x.value = x.next.value;
+      x.next = x.next.next;
+    } else {
+      x = x.next;
+    }
+  }
+  return l;
 }
 
 module.exports = {
